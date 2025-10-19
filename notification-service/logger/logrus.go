86d59a8c3f404c890e7f76cf7387613edbc72307
xadicha <mahkamovahadija1810@gmail.com>
2025-10-19 17:33:26 +0500

    upgrade

---
+++ notification-service/logger/logrus.go
@@ -0,0 +1,22 @@
+package logger
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+func NewLogger() *logrus.Logger {
+	log := logrus.New()
+
+	//stdout/stderr
+	log.SetOutput(os.Stdout)
+
+	// JSON format: 
+	log.SetFormatter(&logrus.JSONFormatter{})
+
+	// Log level: Info yoki Debug
+	log.SetLevel(logrus.InfoLevel)
+
+	return log
+}
---
+++ team-service/internal/repository/postgres/postgres.go
@@ -0,0 +1,27 @@
+package postgres 
+
+import (
+	config "user-service/internal/config"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnectDB(cfg *config.Config) (*sql.DB, error) {
+
+	dataSourceName := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", 
+	cfg.Postgres.Host, 
+	cfg.Postgres.Port, 
+	cfg.Postgres.User, 
+	cfg.Postgres.Password, 
+	cfg.Postgres.Database)
+	db, err := sql.Open("postgres", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
