46b87bb87d08737d9dccb3efaf6a95f0355170eb
xadicha <mahkamovahadija1810@gmail.com>
2025-10-26 20:06:41 +0500

    last

--- scoring-service/migrations/20251026135948_create_scores_table.down.sql
+++ scoring-service/migrations/20251026135948_create_scores_table.down.sql
@@ -1,4 +1,1 @@
-DROP TABLE IF EXISTS scores;
 
-DROP TYPE IF EXISTS scored_by_type;
-DROP TYPE IF EXISTS score_target_type;
--- scoring-service/migrations/20251026135948_create_scores_table.up.sql
+++ scoring-service/migrations/20251026135948_create_scores_table.up.sql
@@ -1,14 +1,14 @@
-CREATE TYPE score_target_type AS ENUM ('TEAM', 'INDIVIDUAL');
 CREATE TYPE scored_by_type AS ENUM ('ORGANIZER', 'ADMIN');
 
 CREATE TABLE scores (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     event_id UUID NOT NULL,
-    target_id UUID NOT NULL,                -- Team ID yoki User ID
-    target_type score_target_type NOT NULL, -- TEAM yoki INDIVIDUAL
+    team_id UUID NOT NULL,                 -- team konteksti
+    user_id UUID NOT NULL,                 -- kimga baho berildi
     points INT NOT NULL CHECK (points >= 0),
     comment TEXT,
-    scored_by_id UUID NOT NULL,             -- kim qo‘ydi
-    scored_by_type scored_by_type NOT NULL, -- ORGANIZER yoki ADMIN
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    scored_by_id UUID NOT NULL,            -- kim baho berdi
+    scored_by_type scored_by_type NOT NULL DEFAULT 'ORGANIZER',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (event_id, team_id, user_id, scored_by_id)
 );
